710973836c8c825477d137de868c45be53d38f80
Mai Trong Nhan <maitrongnhan.001@gmail.com>
2021-06-19 12:02:14 +0700

    responsive component list group chat

--- chat/src/Component/Group-Chat/Header.js
+++ chat/src/Component/Group-Chat/Header.js
@@ -28,7 +28,7 @@ export default class Header extends Component {
             //set InputSreach
             this.setState({
                 StatusSearch: "input-search searching",
-                StatusBtnExit: 'exit-search'
+                StatusBtnExit: ''
             });
             check = false;
         }
@@ -62,8 +62,8 @@ export default class Header extends Component {
     }
 
     componentWillUpdate(prevProps) {
-        if(prevProps.check !== this.props.check){
-            if(!prevProps.check){
+        if (prevProps.check !== this.props.check) {
+            if (!prevProps.check) {
                 this.ExitClick();
             }
         }
@@ -73,7 +73,7 @@ export default class Header extends Component {
         return (
             <nav className="Header-Group-chat">
                 <div className="container">
-                    <div className="row">
+                    <div className="row" id="row-1-Header-Group-Chat">
                         <div className="col-3">
                             <img className="infor-img" src={this.props.MyData}></img>
                         </div>
@@ -95,6 +95,15 @@ export default class Header extends Component {
                         </div>
                     </div>
                     <div className="row">
+                        <div className={"col-1 " + this.state.StatusBtnExit}>
+                            <div className={"exit-search " + this.state.StatusBtnExit}
+                                onClick={this.ExitClick}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="icon-exit" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                                </svg>
+                            </div>
+                        </div>
                         <div className="col-11">
                             <input
                                 role="combobox" placeholder="         Tìm kiếm trên Messenger"
@@ -107,18 +116,11 @@ export default class Header extends Component {
                         </div>
                     </div>
                 </div>
-                <div className={this.state.StatusBtnExit}
-                    onClick={this.ExitClick}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="icon-exit" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-                    </svg>
-                </div>
-                <Manage 
+                <Manage
                     ClickShowListGroup={this.ClickShowListGroup}
-                    StatusManage={this.state.StatusManage} 
+                    StatusManage={this.state.StatusManage}
                     ChangePassword={this.props.ChangePassword}
-                    ChangeAvatar={this.props.ChangeAvatar}/>
+                    ChangeAvatar={this.props.ChangeAvatar} />
             </nav>
         );
     }
