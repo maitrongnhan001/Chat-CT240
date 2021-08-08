37f578eb4b84e86bee1721ab7da649c3441e05d6
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-08-08 15:00:53 +0700

    added function display time

--- chat/src/Component/Chat/Chat.js
+++ chat/src/Component/Chat/Chat.js
@@ -4,9 +4,6 @@ import UserChat from "./UserChat";
 import "./CSS/Chat.scss";
 
 export default class Chat extends Component {
-    constructor(props) {
-        super(props);
-    }
     scrollToBottom = () => {
         this.messagesEnd.scrollIntoView({ behavior: "auto" });
     }
@@ -22,7 +19,8 @@ export default class Chat extends Component {
         return (
             <div className="chat">
                 {this.props.Contents.map((Content) => {
-                    if (this.props.Me.MyName != Content.UserName && Content.UserName != "") {
+                    //render chat to display
+                    if (this.props.Me.MyName !== Content.UserName && Content.UserName !== "") {
                         let PathAvatar = "";
                         //take info user
                         this.props.user.forEach((UserInfor) => {
@@ -36,11 +34,12 @@ export default class Chat extends Component {
                         return <UserChat PathAvatar={PathAvatar}
                             Content={Content} />
                     } else {
-                        if (Content.UserName != "") {
+                        if (Content.UserName !== "") {
                             return <MyChat PathAvatar={this.props.Me.PathAvatar}
                                 Content={Content} />
                         }
                     }
+                    return null;
                 })}
                 <div style={{ float: "left", clear: "both" }}
                     ref={(el) => { this.messagesEnd = el; }}>
